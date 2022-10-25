65eb77289cd2d8fc9308b0609a8896799e8107c1
sharmin1988 <rasnasharmin11@gmail.com>
2022-10-25 08:53:31 +0600

    data required and api create with app.get

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const cors = require ('cors')
 const port = process.env.PORT || 5000
+const courses = require('./data/courses.json')
 
 app.use(cors())
+
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to B-Bio server')
 })
 
-
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 
 
 app.listen(port, () => {
